74121107bedc2af0e75e98f312726dde4d00a7d3
sameer-20 <msnandan.20@gmail.com>
2020-07-22 21:35:06 -0400

    Find count of each letter in a string

---
+++ countLetters.js
@@ -0,0 +1,40 @@
+// Function to compare two strings and check if they are equal or not
+const assertEqual = function(actual, expected) {
+  if (actual !== expected)
+    return console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  else
+    return console.log(`🙂Assertion Passed: ${actual} === ${expected}`);
+};
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countLetters = function(inputString) {
+  const results = {};
+  const newString = inputString.split(" ").join("");
+  console.log(newString);
+  for (const character of newString) {
+    //console.log(character);
+      if (results[character]) {
+        results[character] += 1;
+      } else {
+        results[character] = 1;
+      }
+  }
+  return results;
+};
+
+
+// TEST CODE
+
+let result1 = countLetters("twinkle twinkle little star");
+console.log(result1);
+
+ assertEqual(result1["t"], 5); //Pass
+ assertEqual(result1["n"], 3); //Fail
+ assertEqual(result1["r"], 1); //Pass
+
+result1 = countLetters("lighthouse in the house");
+console.log(result1);
+ 
