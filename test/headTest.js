1387901c79392ee984435a4d4e612405de74e749
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-07-27 19:39:19 +0000

    Refactored headTest.js using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,15 +1,35 @@
 
 const assertEqual = require ('../assertEqual');
-const head = require ('../head.js');
+const head = require ('../head');
+const assert = require('chai').assert;
+
 
 // TEST CODE for head.js
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([4,5,6,7]), 5);
-assertEqual(head(["Hello Dear", "Hello", "Labs"]), "Hello");
-assertEqual(head(["5",6,7]), 5);
-assertEqual(head(["hEllo", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
-assertEqual(head([]), "");
-assertEqual(head(["Hello Dear"]), "Hello Dear");
-assertEqual(head([4]), 4);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);  
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+
+  it("do not return '5' for [4,5,6,7]", () => {
+    assert.notEqual(head([4,5,6,7]), 5); 
+  });
+
+  it("returns 'Hello Dear' for ['Hello Dear']", () => {
+    assert.strictEqual(head(['Hello Dear']), 'Hello Dear'); 
+  });
+
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+
+  
+});
